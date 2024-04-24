048b473f7fbc042f6bab682c99aa9bb32e0cb796
Sasha <a.zhogol@mail.ru>
2024-04-25 00:02:42 +0300

    add tests

--- src/test/java/com/wiki/controller/CommentControllerTest.java
+++ src/test/java/com/wiki/controller/CommentControllerTest.java
@@ -1,4 +1,106 @@
 package com.wiki.controller;
 
+import com.wiki.dto.CommentDto;
+import com.wiki.model.Comment;
+import com.wiki.service.CommentService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 public class CommentControllerTest {
+  private MockMvc mockMvc;
+
+  @Mock
+  private CommentService commentService;
+  @BeforeEach
+  public void setup() {
+    MockitoAnnotations.openMocks(this);
+    CommentController commentController = new CommentController(commentService);
+    mockMvc = MockMvcBuilders.standaloneSetup(commentController).build();
+  }
+
+  @Test
+  public void testFindAllComment() throws Exception {
+    List<Comment> commentList = new ArrayList<>();
+    commentList.add(new Comment());
+    commentList.add(new Comment());
+    when(commentService.findAllComment()).thenReturn(commentList);
+
+    MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/Comment")
+                    .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andReturn();
+
+    String responseContent = result.getResponse().getContentAsString();
+    Assertions.assertNotNull(responseContent);
+  }
+
+  @Test
+  public void testSaveComment() throws Exception {
+    // Arrange
+    CommentDto commentDto = new CommentDto();
+    commentDto.setAuthor("Cats");
+    commentDto.setText("Test");
+    Comment savedComment = new Comment();
+    savedComment.setAuthor(commentDto.getAuthor());
+    savedComment.setText(commentDto.getText());
+    when(commentService.saveComment(any(CommentDto.class))).thenReturn(savedComment);
+
+    MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/Comment/saveComment")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content("{\"author\":\"Cats\",\"text\":\"Test\"}"))
+            .andExpect(status().isOk())
+            .andReturn();
+    String responseContent = result.getResponse().getContentAsString();
+    Assertions.assertNotNull(responseContent);
+  }
+
+  @Test
+  public void testFindById() throws Exception {
+    Long commentId = 1L;
+    Comment comment = new Comment();
+    comment.setId(commentId);
+    when(commentService.findComment(commentId)).thenReturn(comment);
+
+    MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/Comment/findById?id=1")
+                    .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andReturn();
+
+    String responseContent = result.getResponse().getContentAsString();
+    Assertions.assertNotNull(responseContent);
+  }
+
+  @Test
+  public void testUpdateComment() throws Exception {
+    CommentDto commentDto = new CommentDto();
+    commentDto.setAuthor("Cats");
+    commentDto.setText("Test");
+    Comment updatedComment = new Comment();
+    updatedComment.setAuthor(commentDto.getAuthor());
+    updatedComment.setText(commentDto.getText());
+    when(commentService.updateComment(any(CommentDto.class))).thenReturn(updatedComment);
+
+    MvcResult result = mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/Comment/updateComment")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content("{\"author\":\"Cats\",\"text\":\"Updated test\"}"))
+            .andExpect(status().isOk())
+            .andReturn();
+
+    String responseContent = result.getResponse().getContentAsString();
+    Assertions.assertNotNull(responseContent);
+  }
 }
--- src/test/java/com/wiki/controller/WikiControllerTest.java
+++ src/test/java/com/wiki/controller/WikiControllerTest.java
@@ -1,4 +1,108 @@
 package com.wiki.controller;
 
+import com.wiki.dto.WikiDto;
+import com.wiki.model.Wiki;
+import com.wiki.service.WikiService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 public class WikiControllerTest {
+  private MockMvc mockMvc;
+
+  @Mock
+  private WikiService wikiService;
+
+  @BeforeEach
+  public void setup() {
+    MockitoAnnotations.initMocks(this);
+    WikiController wikiController = new WikiController(wikiService);
+    mockMvc = MockMvcBuilders.standaloneSetup(wikiController).build();
+  }
+
+  @Test
+  public void testFindAllWiki() throws Exception {
+    // Arrange
+    Wiki wiki1 = new Wiki();
+    Wiki wiki2 = new Wiki();
+    List<Wiki> wikiList = Arrays.asList(wiki1, wiki2);
+    when(wikiService.findAllWiki()).thenReturn(wikiList);
+
+    MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/Wiki")
+                    .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andReturn();
+  }
+
+  @Test
+  public void testSaveWiki() throws Exception {
+    Wiki savedWiki = new Wiki();
+    when(wikiService.saveWiki(any(WikiDto.class))).thenReturn(savedWiki);
+    MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/Wiki/saveWiki")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content("{\"property\":\"value\"}"))
+            .andExpect(status().isOk())
+            .andReturn();
+
+    String responseContent = result.getResponse().getContentAsString();
+  }
+
+  @Test
+  public void testFindByRequest() throws Exception {
+    String request = "Test Request";
+    Wiki wiki = new Wiki();
+    when(wikiService.findByRequest(request)).thenReturn(wiki);
+    MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/Wiki/findByRequest?request=Test Request")
+                    .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andReturn();
+  }
+
+  @Test
+  public void testUpdateWiki() throws Exception {
+    Wiki updatedWiki = new Wiki();
+    when(wikiService.updateWiki(any(WikiDto.class))).thenReturn(updatedWiki);
+    MvcResult result = mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/Wiki/updateWiki")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content("{\"property\":\"value\"}"))
+            .andExpect(status().isOk())
+            .andReturn();
+  }
+
+  @Test
+  public void testDeleteWiki() throws Exception {
+    String request = "Test Request";
+    mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/Wiki/deleteWiki?request=Test Request")
+                    .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
+    verify(wikiService).deleteWiki(request);
+  }
+
+  @Test
+  public void testFindByRequestWikiAndAuthor() throws Exception {
+    String requestWiki = "Test Request";
+    String author = "Author";
+    Wiki wiki = new Wiki();
+    when(wikiService.findByRequestWikiAndAuthor(requestWiki, author)).thenReturn(wiki);
+    MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/Wiki/findByRequestAndAuthor")
+                    .param("requestWiki", requestWiki)
+                    .param("author", author)
+                    .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andReturn();
+  }
 }
