165ac71146b2288555ba16f1f371e863e355b1be
Sasha <a.zhogol@mail.ru>
2024-04-25 00:57:59 +0300

    add tests

--- src/test/java/com/wiki/service/CommentServiceTest.java
+++ src/test/java/com/wiki/service/CommentServiceTest.java
@@ -1,5 +1,131 @@
 package com.wiki.service;
 
-class CommentServiceTest {
+import com.wiki.cache.DataCache;
+import com.wiki.dto.CommentDto;
+import com.wiki.mapper.CommentMapper;
+import com.wiki.model.Comment;
+import com.wiki.model.Wiki;
+import com.wiki.repository.CommentRepository;
+import com.wiki.repository.WikiRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+class CommentServiceTest {
+  private CommentService commentService;
+
+  @Mock
+  private CommentRepository commentRepository;
+
+  @Mock
+  private WikiRepository wikiRepository;
+
+  @Mock
+  private DataCache commentCache;
+
+  @BeforeEach
+  public void setup() {
+    MockitoAnnotations.initMocks(this);
+    commentService = new CommentService(commentRepository, wikiRepository, commentCache);
+  }
+
+  @Test
+  public void testFindAllComment_FromCache() {
+    List<Comment> commentList = new ArrayList<>();
+    commentList.add(new Comment());
+    when(commentCache.get("all")).thenReturn(commentList);
+    List<Comment> result = commentService.findAllComment();
+    assertEquals(commentList, result);
+  }
+
+  @Test
+  public void testFindAllComment_FromDatabase() {
+    List<Comment> commentList = new ArrayList<>();
+    commentList.add(new Comment());
+    when(commentRepository.findAll()).thenReturn(commentList);
+    when(commentCache.get("all")).thenReturn(null);
+    List<Comment> result = commentService.findAllComment();
+    assertEquals(commentList, result);
+    verify(commentCache).put(eq("all"), any());
+  }
+
+  @Test
+  public void testFindComment_FromCache() {
+    Long commentId = 1L;
+    Comment comment = new Comment();
+    when(commentCache.get(commentId.toString())).thenReturn(comment);
+    Comment result = commentService.findComment(commentId);
+    assertEquals(comment, result);
+  }
+
+  @Test
+  public void testFindComment_FromDatabase() {
+    Long commentId = 1L;
+    Comment comment = new Comment();
+    when(commentRepository.findCommentById(commentId)).thenReturn(comment);
+    when(commentCache.get(commentId.toString())).thenReturn(null);
+
+    Comment result = commentService.findComment(commentId);
+
+    assertEquals(comment, result);
+    verify(commentCache).put(eq(commentId.toString()), any());
+  }
+
+  @Test
+  public void testDeleteComment_CommentFound() {
+    Long commentId = 1L;
+    Comment comment = new Comment();
+    List<Comment> commentList = new ArrayList<Comment>();
+    commentList.add(comment);
+    Wiki wiki = new Wiki();
+    wiki.setCommentList(commentList);
+    when(commentRepository.findCommentById(commentId)).thenReturn(comment);
+    when(wikiRepository.findWikiByCommentListContains(comment)).thenReturn(wiki);
+
+    commentService.deleteComment(commentId);
+
+    verify(wikiRepository).findWikiByCommentListContains(comment);
+    assertTrue(wiki.getCommentList().isEmpty());
+    verify(commentRepository).deleteById(commentId);
+    verify(commentCache).remove(commentId.toString());
+  }
+
+  @Test
+  public void testUpdateComment() {
+    Long commentId = 1L;
+    String updatedText = "Updated Text";
+    CommentDto commentDto = new CommentDto();
+    commentDto.setId(commentId);
+    commentDto.setText(updatedText);
+    Comment comment = new Comment();
+    when(commentRepository.findCommentById(commentId)).thenReturn(comment);
+    when(commentRepository.save(comment)).thenReturn(comment);
+
+    Comment result = commentService.updateComment(commentDto);
+
+    assertEquals(updatedText, result.getText());
+    verify(commentCache).remove(commentId.toString());
+    verify(commentCache).put(eq("all"), any(Comment.class));
+  }
+
+  @Test
+  public void testSaveComment() {
+    CommentDto commentDto = new CommentDto();
+    Comment comment = CommentMapper.toEntity(commentDto);
+    when(commentRepository.save(comment)).thenReturn(comment);
+    Comment result = commentService.saveComment(commentDto);
+    CommentDto resultDto=CommentMapper.toDto(result);
+    resultDto.setId(commentDto.getId());
+    assertEquals(commentDto.getText(), resultDto.getText());
+    verify(commentCache).put(eq("all"), any(Comment.class));
+    verify(commentCache).put("all", result);
+  }
 }
--- src/test/java/com/wiki/service/WikiServiceTest.java
+++ src/test/java/com/wiki/service/WikiServiceTest.java
@@ -1,4 +1,154 @@
 package com.wiki.service;
+import com.wiki.cache.DataCache;
+import com.wiki.dto.WikiDto;
+import com.wiki.mapper.WikiMapper;
+import com.wiki.model.Comment;
+import com.wiki.model.Wiki;
+import com.wiki.repository.CommentRepository;
+import com.wiki.repository.WikiRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 class WikiServiceTest {
+  private WikiService wikiService;
+
+  @Mock
+  private CommentRepository commentRepository;
+
+  @Mock
+  private WikiRepository wikiRepository;
+
+  @Mock
+  private DataCache wikiCache;
+
+  @BeforeEach
+  public void setup() {
+    MockitoAnnotations.initMocks(this);
+    wikiService = new WikiService(commentRepository, wikiRepository, wikiCache);
+  }
+
+  @Test
+  public void testFindAllWiki_FromCache() {
+    List<Wiki> wikiList = new ArrayList<>();
+    wikiList.add(new Wiki());
+    when(wikiCache.get("all")).thenReturn(wikiList);
+
+    List<Wiki> result = wikiService.findAllWiki();
+
+    assertEquals(wikiList, result);
+  }
+
+  @Test
+  public void testFindAllWiki_FromDatabase() {
+    // Arrange
+    List<Wiki> wikiList = new ArrayList<>();
+    wikiList.add(new Wiki());
+    when(wikiRepository.findAll()).thenReturn(wikiList);
+    when(wikiCache.get("all")).thenReturn(null);
+
+    List<Wiki> result = wikiService.findAllWiki();
+
+    assertEquals(wikiList, result);
+    verify(wikiCache).put("all", wikiList);
+  }
+
+  @Test
+  public void testFindByRequest_FromCache() {
+    String requestWiki = "test";
+    Wiki wiki = new Wiki();
+    when(wikiCache.get(requestWiki)).thenReturn(wiki);
+
+    Wiki result = wikiService.findByRequest(requestWiki);
+
+    assertEquals(wiki, result);
+  }
+
+  @Test
+  public void testFindByRequest_FromDatabase() {
+    String requestWiki = "test";
+    Wiki wiki = new Wiki();
+    when(wikiRepository.findWikiByRequestWiki(requestWiki)).thenReturn(wiki);
+    when(wikiCache.get(requestWiki)).thenReturn(null);
+    Wiki result = wikiService.findByRequest(requestWiki);
+    assertEquals(wiki, result);
+    verify(wikiCache).put(requestWiki, wiki);
+  }
+
+  @Test
+  public void testDeleteWiki_WikiNotFound() {
+    String requestWiki = "test";
+    when(wikiRepository.findWikiByRequestWiki(requestWiki)).thenReturn(null);
+    wikiService.deleteWiki(requestWiki);
+    verify(wikiRepository).findWikiByRequestWiki(requestWiki);
+  }
+
+  @Test
+  public void testDeleteWiki_WikiFound() {
+    String requestWiki = "test";
+    Wiki wiki = new Wiki();
+    List<Comment> commentList = new ArrayList<>();
+    commentList.add(new Comment());
+    wiki.setCommentList(commentList);
+    when(wikiRepository.findWikiByRequestWiki(requestWiki)).thenReturn(wiki);
+    wikiService.deleteWiki(requestWiki);
+    verify(wikiRepository).findWikiByRequestWiki(requestWiki);
+    verify(commentRepository).deleteAll(commentList);
+    verify(wikiRepository).delete(wiki);
+//    verify(wikiCache).remove(requestWiki);
+  }
+
+  @Test
+  public void testUpdateWiki() {
+    String requestWiki = "test";
+    String updatedAnswer = "Updated";
+    WikiDto wikiDto = new WikiDto();
+    wikiDto.setRequestWiki(requestWiki);
+    wikiDto.setAnswerWiki(updatedAnswer);
+    Wiki wiki = new Wiki();
+    when(wikiRepository.findWikiByRequestWiki(requestWiki)).thenReturn(wiki);
+    Wiki result = wikiService.updateWiki(wikiDto);
+    assertEquals(updatedAnswer, result.getAnswerWiki());
+    verify(wikiCache).remove(requestWiki);
+  }
+
+  @Test
+  public void testSaveWiki() {
+    WikiDto wikiDto = new WikiDto();
+    Wiki wiki = WikiMapper.toEntity(wikiDto);
+    when(wikiRepository.save(wiki)).thenReturn(wiki);
+    Wiki result = wikiService.saveWiki(wikiDto);
+    assertEquals(result, wikiDto.getAnswerWiki());
+  }
+
+  @Test
+  public void testFindByRequestWikiAndAuthor_FromCache() {
+    String requestWiki = "test";
+    String author = "Sasha";
+    String cacheKey = requestWiki + "_" + author;
+    Wiki wiki = new Wiki();
+    when(wikiCache.get(cacheKey)).thenReturn(wiki);
+    Wiki result = wikiService.findByRequestWikiAndAuthor(requestWiki, author);
+    assertEquals(wiki, result);
+  }
+
+  @Test
+  public void testFindByRequestWikiAndAuthor_FromDatabase() {
+    String requestWiki = "test";
+    String author = "Sasha";
+    String cacheKey = requestWiki + "_" + author;
+    Wiki wiki = new Wiki();
+    when(wikiRepository.findWikiByRequestWikiAndAuthor(requestWiki, author)).thenReturn(wiki);
+    when(wikiCache.get(cacheKey)).thenReturn(null);
+    Wiki result = wikiService.findByRequestWikiAndAuthor(requestWiki, author);
+    assertEquals(wiki, result);
+    verify(wikiCache).put(cacheKey, wiki);
+  }
 }
